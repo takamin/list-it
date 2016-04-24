eae941140aaa21db0fe90d695607f21b77d72c96
Koji Takami <vzg03566@gmail.com>
2016-04-24 10:57:34 +0900

    issue4: The autoAlign mode is available
    In this mode, the data in cell will be aligned in automatic depending on its type.
    The number will be aligned to the right taking account of its decimal point.
    
    * Type : boolean
    * Default setting : false

--- test/test.js
+++ test/test.js
@@ -72,4 +72,95 @@
             });
         });
     });
+    describe("When opt.autoAlign", function() {
+        describe("is true,", function() {
+            it('integers are aligned to right', function() {
+                var buffer = new listit.buffer({autoAlign:true});
+                buffer
+                    .d(1).d("2").d("3").nl()
+                    .d("10").d(20).d("30").nl()
+                    .d("997").d("998").d(999).nl()
+                    .d(-1).d(-2).d(-333);
+                assert.equal(buffer.toString(),
+                        "  1 2   3   \n" +
+                        "10   20 30  \n" +
+                        "997 998  999\n" +
+                        " -1  -2 -333");
+            });
+            it('taking of decimal point #1', function() {
+                var buffer = new listit.buffer({autoAlign:true});
+                buffer
+                    .d(1.5).nl()
+                    .d("10").nl()
+                    .d("997").nl()
+                    .d(-1.125);
+                assert.equal(buffer.toString(),
+                        " 1.5  \n" +
+                        "10    \n" +
+                        "997   \n" +
+                        "-1.125");
+            });
+            it('taking of decimal point #2', function() {
+                var buffer = new listit.buffer({autoAlign:true});
+                buffer
+                    .d("2").nl()
+                    .d(20.125).nl()
+                    .d("998").nl()
+                    .d(-2.25);
+                assert.equal(buffer.toString(),
+                        "2     \n" +
+                        "20.125\n" +
+                        "998   \n" +
+                        "-2.25 ");
+            });
+            it('taking of decimal point #3', function() {
+                var buffer = new listit.buffer({autoAlign:true});
+                buffer
+                    .d("3").nl()
+                    .d("30").nl()
+                    .d(999.0625).nl()
+                    .d(-333.5);
+                assert.equal(buffer.toString(),
+                        "3        \n" +
+                        "30       \n" +
+                        " 999.0625\n" +
+                        "-333.5   ");
+            });
+            it('long string is mixed in', function() {
+                var buffer = new listit.buffer({autoAlign:true});
+                buffer
+                    .d("3").nl()
+                    .d("ABCDEFGHIJKLMN").nl()
+                    .d(999.0625).nl()
+                    .d(-333.5);
+                assert.equal(buffer.toString(),
+                        "3             \n" +
+                        "ABCDEFGHIJKLMN\n" +
+                        "      999.0625\n" +
+                        "     -333.5   ");
+            });
+            it('representation of the number less than 1', function() {
+                var buffer = new listit.buffer({autoAlign:true});
+                buffer
+                    .d(.5).nl()
+                    .d(-.5).nl()
+                    .d(-125.125);
+                assert.equal(buffer.toString(),
+                        "   0.5  \n" +
+                        "  -0.5  \n" +
+                        "-125.125");
+            });
+            it('fractional representation of an integer', function() {
+                var buffer = new listit.buffer({autoAlign:true});
+                buffer
+                    .d(-987).nl()
+                    .d(987).nl()
+                    .d(-1250.125);
+                assert.equal(buffer.toString(),
+                        " -987.0  \n" +
+                        "  987.0  \n" +
+                        "-1250.125");
+            });
+        });
+    });
 }());
